e3580303cdaf99cae8d9cb346a77b55b76e3af46
Mahnoor Ali <80174852+mahnoor-ali@users.noreply.github.com>
2025-01-22 15:51:54 +0500

    Add files via upload

---
+++ IPLoggingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Middlewares.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class IPLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public IPLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    // Like other middlewares in Program.cs use app.UseSomeXyz() those are also passing IApplicationBuilder object which is 'app'
+    public static class IPLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseIPLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<IPLoggingMiddleware>();
+        }
+    }
+}
